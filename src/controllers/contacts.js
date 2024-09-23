8a55f6543bdb9aed1c89f9ffddcd605db90c7cb5
YuriiGreg <yuriyf888@gmail.com>
2024-09-23 15:53:03 +0200

    nodejs-hw-03...

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -2,37 +2,68 @@ const contactsService = require('../services/contacts');
 const createError = require('http-errors');
 
 const getAllContacts = async (req, res) => {
-    const contacts = await contactsService.getAll();
-    res.json({ status: 200, data: contacts });
+    try {
+        const contacts = await contactsService.getAll();
+        res.json({ status: 200, data: contacts });
+    } catch (error) {
+        throw createError(500, 'Failed to retrieve contacts');
+    }
 };
 
 const getContactById = async (req, res, next) => {
-    const contact = await contactsService.getById(req.params.contactId);
-    if (!contact) {
-        throw createError(404, 'Contact not found');
+    try {
+        const contact = await contactsService.getById(req.params.contactId);
+        if (!contact) {
+            throw createError(404, 'Contact not found');
+        }
+        res.json({ status: 200, data: contact });
+    } catch (error) {
+        next(error); 
     }
-    res.json({ status: 200, data: contact });
 };
 
-const createContact = async (req, res) => {
-    const contact = await contactsService.create(req.body);
-    res.status(201).json({ status: 201, message: "Successfully created a contact!", data: contact });
+const createContact = async (req, res, next) => {
+    const { name, phoneNumber, contactType } = req.body;
+
+  
+    if (!name || !phoneNumber || !contactType) {
+        return next(createError(400, 'Missing required fields: name, phoneNumber, or contactType.'));
+    }
+
+    try {
+        const contact = await contactsService.create(req.body);
+        res.status(201).json({
+            status: 201,
+            message: "Successfully created a contact!",
+            data: contact,
+        });
+    } catch (error) {
+        next(createError(500, 'Failed to create contact')); 
+    }
 };
 
 const updateContact = async (req, res, next) => {
-    const contact = await contactsService.update(req.params.contactId, req.body);
-    if (!contact) {
-        throw createError(404, 'Contact not found');
+    try {
+        const contact = await contactsService.update(req.params.contactId, req.body);
+        if (!contact) {
+            throw createError(404, 'Contact not found');
+        }
+        res.json({ status: 200, message: "Successfully patched a contact!", data: contact });
+    } catch (error) {
+        next(error);
     }
-    res.json({ status: 200, message: "Successfully patched a contact!", data: contact });
 };
 
 const deleteContact = async (req, res, next) => {
-    const contact = await contactsService.remove(req.params.contactId);
-    if (!contact) {
-        throw createError(404, 'Contact not found');
+    try {
+        const contact = await contactsService.remove(req.params.contactId);
+        if (!contact) {
+            throw createError(404, 'Contact not found');
+        }
+        res.status(204).send();
+    } catch (error) {
+        next(error);
     }
-    res.status(204).send();
 };
 
 module.exports = {
@@ -42,4 +73,5 @@ module.exports = {
     updateContact,
     deleteContact
 };
+
 
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,15 +1,72 @@
-const contacts = [];
+const Contact = require('../models/contactModel');
 const createError = require('http-errors');
 
-const deleteContactById = async (contactId) => {
-  const contactIndex = contacts.findIndex(contact => contact.id === Number(contactId));
-  if (contactIndex === -1) {
-    throw createError(404, 'Contact not found');
-  }
+const getContactsFromDB = async () => {
+    try {
+        return await Contact.find({});
+    } catch (error) {
+        throw createError(500, 'Failed to retrieve contacts');
+    }
+};
 
-  contacts.splice(contactIndex, 1);
+const getContactByIdFromDB = async (contactId) => {
+    try {
+        return await Contact.findById(contactId);
+    } catch (error) {
+        throw createError(500, 'Failed to retrieve contact');
+    }
+};
+
+const create = async (contactData) => {
+    const newContact = new Contact(contactData);
+    try {
+        return await newContact.save(); 
+    } catch (error) {
+        throw createError(500, 'Failed to create contact');
+    }
+};
+
+const deleteContactById = async (contactId) => {
+    try {
+        const contact = await Contact.findByIdAndDelete(contactId);
+        if (!contact) {
+            throw createError(404, 'Contact not found');
+        }
+        return contact; 
+    } catch (error) {
+        throw createError(500, 'Failed to delete contact');
+    }
+};
+
+const update = async (contactId, contactData) => {
+    try {
+        const updatedContact = await Contact.findByIdAndUpdate(contactId, contactData, { new: true });
+        if (!updatedContact) {
+            throw createError(404, 'Contact not found');
+        }
+        return updatedContact; 
+    } catch (error) {
+        throw createError(500, 'Failed to update contact');
+    }
+};
+
+const remove = async (contactId) => {
+    try {
+        const contact = await Contact.findByIdAndDelete(contactId);
+        if (!contact) {
+            throw createError(404, 'Contact not found');
+        }
+        return contact; 
+    } catch (error) {
+        throw createError(500, 'Failed to delete contact');
+    }
 };
 
 module.exports = {
-  deleteContactById,
+    getContactsFromDB,
+    getContactByIdFromDB,
+    create,
+    deleteContactById,
+    update,
+    remove, 
 };
