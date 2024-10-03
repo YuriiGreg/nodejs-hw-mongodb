0fc0f521b19e05d48de795ad7253ab88b2721f95
YuriiGreg <yuriyf888@gmail.com>
2024-10-03 15:34:20 +0200

    Update contactSchemas.js

--- src/schemas/contactSchemas.js
+++ src/schemas/contactSchemas.js
@@ -1,19 +1,23 @@
 const Joi = require('joi');
 
-const contactSchema = Joi.object({
+const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   phoneNumber: Joi.string().min(3).max(20).required(),
-  email: Joi.string().email().optional(),
-  isFavourite: Joi.boolean().optional(),
-  contactType: Joi.string().valid('work', 'home', 'personal').default('personal'),
+  email: Joi.string().email(),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid('work', 'home', 'personal').default('personal')
 });
 
 const updateContactSchema = Joi.object({
-  name: Joi.string().min(3).max(20).optional(),
-  phoneNumber: Joi.string().min(3).max(20).optional(),
-  email: Joi.string().email().optional(),
-  isFavourite: Joi.boolean().optional(),
-  contactType: Joi.string().valid('work', 'home', 'personal').optional(),
-});
+  name: Joi.string().min(3).max(20),
+  phoneNumber: Joi.string().min(3).max(20),
+  email: Joi.string().email(),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid('work', 'home', 'personal')
+}).or('name', 'phoneNumber', 'email', 'isFavourite', 'contactType');
 
-module.exports = { contactSchema, updateContactSchema };
+module.exports = {
+  createContactSchema,
+  updateContactSchema
+};
+
