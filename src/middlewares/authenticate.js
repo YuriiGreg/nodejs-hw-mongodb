ad65da20077f996f2f052b6556d149079b0c155f
YuriiGreg <yuriyf888@gmail.com>
2024-10-07 23:51:08 +0200

    nodejs-hw-05

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -6,39 +6,41 @@ const Session = require('../models/sessionModel');
 const authenticate = async (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
-    
+
    
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
       throw createError(401, 'Authorization header missing or incorrect');
     }
 
-    
     const token = authHeader.split(' ')[1];
 
-    
+  
     const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
 
   
-    const user = await User.findById(decoded.id);
+    const user = await User.findById(decoded.userId); 
     if (!user) {
       throw createError(401, 'User not found');
     }
 
-    
     const session = await Session.findOne({ accessToken: token });
     if (!session || session.accessTokenValidUntil < Date.now()) {
       throw createError(401, 'Access token expired');
     }
 
-    
     req.user = user;
     next(); 
   } catch (error) {
+    
     if (error.name === 'TokenExpiredError') {
       return next(createError(401, 'Access token expired'));
+    }
+    if (error.name === 'JsonWebTokenError') {
+      return next(createError(401, 'Invalid token'));
     }
     next(createError(401, error.message || 'Unauthorized'));
   }
 };
 
 module.exports = authenticate;
+
--- src/middlewares/notFoundHandler.js
+++ src/middlewares/notFoundHandler.js
@@ -1,7 +1,7 @@
 const createError = require('http-errors');
 
 const notFoundHandler = (req, res, next) => {
-    next(createError(404, 'Route not found'));
+  res.status(404).json({ status: 404, message: "Route not found" });
 };
 
 module.exports = notFoundHandler;
